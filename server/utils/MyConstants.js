2f3a643134e7fb7dbc8585466b5de72bac7e7343
Nguyễn Tất Hiển <tathienbadao@gmail.com>
2023-10-14 18:30:55 +0700

    shoppingonline project

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+  DB_SERVER: "cluster.aqbpxyp.mongodb.net",
+  DB_USER: "tathien2003",
+  DB_PASS: "qh4RNbP8FdYOHbBr",
+  DB_DATABASE: "shoppingonline",
+  EMAIL_USER: "tathien2003@hotmail.com", // Microsoft mail service
+  EMAIL_PASS: "Hien0908127818",
+  JWT_SECRET: "27052003",
+
+  JWT_EXPIRES: 60 * 60 * 1000, // in milliseconds
+};
+module.exports = MyConstants;
